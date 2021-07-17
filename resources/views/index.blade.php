f4d3bcd33d85f0db01ef4345c911779ba82d22b2
Darren Henderson <darrenh@ccsj.edu>
2021-07-17 01:16:05 -0500

    First commit.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel 8 - Vue 2 - Highcharts</title>
+</head>
+<body>
+    <div id="app">
+        <h1>Hello</h1>
+        <!-- <example-component message="Danger Danger Will Robinson!!"></example-component> -->
+    
+        <!-- <hc-solid-gauge-component></hc-solid-gauge-component> -->
+        <hc-line-chart-component></hc-line-chart-component>
+        <hc-column-chart-component></hc-column-chart-component>
+        <hc-pie-chart-component></hc-pie-chart-component>
+        <hc-stacked-column-chart-component></hc-stacked-column-chart-component>
+        <hc-stacked-percentage-column-chart-component></hc-stacked-percentage-column-chart-component>
+    <script src="js/app.js"></script>
+
+</body>
+</html>
